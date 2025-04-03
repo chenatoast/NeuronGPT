dc683964590dc1f6ee08f704c7cc98b9b43d2025
Manasvi Jain <manasvijain152003@gmail.com>
2025-04-03 22:38:15 +0530

    comments and information

--- bigram.py
+++ bigram.py
@@ -5,7 +5,7 @@ import torch.nn as nn
 from torch.nn import functional as F
 
 # hyperparameters
-batch_size = 32 # how many independent sequences will we process in parallel?
+batch_size = 32 # how many independent sequences will be processed in parallel
 block_size = 8 # what is the maximum context length for predictions?
 max_iters = 3000
 eval_interval = 300
@@ -14,7 +14,7 @@ device = 'cuda' if torch.cuda.is_available() else 'cpu'
 eval_iters = 200
 # ------------
 
-torch.manual_seed(1337)
+torch.manual_seed(1337)  #so that we get the same values while working on seuences
 
 with open('input.txt', 'r', encoding='utf-8') as f:
     text = f.read()
